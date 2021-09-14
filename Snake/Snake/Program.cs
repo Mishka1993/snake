75a3dea81023669e13c3bf2737217716b123a01a
Mishka1993 <misha220993@gmail.com>
2021-09-14 15:50:56 +0400

    test

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,7 @@ namespace Snake
         {
            Console.SetBufferSize(120, 30);
 
-            HorizontalLine upLine = new HorizontalLine(0,119,0,'-');
+            HorizontalLine upLine = new HorizontalLine(0, 119, 0, '-');
             HorizontalLine downLine = new HorizontalLine(0, 119, 29, '-');
             VerticalLine leftLine = new VerticalLine(0, 29, 0, '|');
             VerticalLine rightLine = new VerticalLine(0, 29, 119, '|');
